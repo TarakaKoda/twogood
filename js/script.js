ae4ac2a8899bd8a049f71236314bed7971d60577
TarakaKoda <taraka.koda@gmail.com>
2023-10-11 23:14:24 +0530

    Build the product components.

--- js/script.js
+++ js/script.js
@@ -1,29 +1,34 @@
-function videoConAnimation() {
-    var videoCon = document.querySelector(".video-container");
-var playBtn = document.querySelector(".play");
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('.main'),
+    smooth: true
+});
 
-videoCon.addEventListener("mouseenter", function() {
-    gsap.to(playBtn, {
-        scale: 1,
-        opacity: 1,
-    })
-})
+// function videoConAnimation() {
+//     var videoCon = document.querySelector(".video-container");
+//     var playBtn = document.querySelector(".play");
 
-videoCon.addEventListener("mouseleave", function() {
-    gsap.to(playBtn, {
-        scale: 0,
-        opacity: 0,
-    })
-})
+//     videoCon.addEventListener("mouseenter", function() {
+//         gsap.to(playBtn, {
+//             scale: 1,
+//             opacity: 1,
+//         })
+//     })
 
-videoCon.addEventListener("mousemove", function(dets) {
-    gsap.to(playBtn, {
-        left: dets.x - 45,
-        top: dets.y - 45,
-    })
-})
-}
-videoConAnimation();
+//     videoCon.addEventListener("mouseleave", function() {
+//         gsap.to(playBtn, {
+//             scale: 0,
+//             opacity: 0,
+//         })
+//     })
+
+//     videoCon.addEventListener("mousemove", function(dets) {
+//         gsap.to(playBtn, {
+//             left: dets.x - 45,
+//             top: dets.y - 45,
+//         })
+//     })
+// }
+// videoConAnimation();
 
 function loadingAnimation() {
     gsap.from(".title-container .change", {
@@ -45,12 +50,12 @@ function loadingAnimation() {
         duration: 0.4
     });
 
-    gsap.from(".title-course .video-container video", {
-        scale: 0.9,
-        opacity: 0,
-        delay: 1.2,
-        duration: 0.4
-    });
+    // gsap.from(".title-course .video-container video", {
+    //     scale: 0.9,
+    //     opacity: 0,
+    //     delay: 1.2,
+    //     duration: 0.4
+    // });
 
     gsap.from(".image-container img", {
         y:100,
@@ -68,3 +73,31 @@ function loadingAnimation() {
     })
 }
 loadingAnimation();
+
+function scrollTrigger(element, scale, opacity, delay, duration) {
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            // Execute your GSAP animation when the element is in the viewport
+            gsap.from(element, {
+              scale,
+              opacity,
+              delay,
+              duration
+            });
+            // Once the animation is triggered, you may want to unobserve the element
+            observer.unobserve(entry.target);
+          }
+        });
+      });
+      
+    const elementToObserve = document.querySelector(element);
+    if (elementToObserve) {
+    observer.observe(elementToObserve);
+    }
+}
+
+scrollTrigger("#product1", scale=1.1, opacity=0, delay=0.2, duration=0.4);
+scrollTrigger("#product2", scale=1.1, opacity=0, delay=0.4, duration=0.4);
+scrollTrigger("#product3", scale=1.1, opacity=0, delay=0.6, duration=0.4);
+
