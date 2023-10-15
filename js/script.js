37238ab22789805f294d7de54b77d8c125e98494
TarakaKoda <taraka.koda@gmail.com>
2023-10-15 10:01:11 +0530

    Made footer responsive for all devices.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,36 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('.main'),
+function locomotiveAnimation() {
+  gsap.registerPlugin(ScrollTrigger);
+
+  // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector(".main"),
     smooth: true
-});
+  });
+  // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+  locoScroll.on("scroll", ScrollTrigger.update);
+
+  // tell ScrollTrigger to use these proxy methods for the ".main" element since Locomotive Scroll is hijacking things
+  ScrollTrigger.scrollerProxy(".main", {
+    scrollTop(value) {
+      return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+    getBoundingClientRect() {
+      return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+    },
+    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+    pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
+  });
+
+  // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+  // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+  ScrollTrigger.refresh();
+
+}
+
+locomotiveAnimation();
 
 // function videoConAnimation() {
 //     var videoCon = document.querySelector(".video-container");
@@ -85,15 +114,36 @@ function loadingAnimation() {
       duration: 0.4,
     });
     gsap.from("#product1", {
-      opacity: 0,
-      y:100,
+      opacity:0,
       delay: 1.6,
       duration: 0.4,
     });
 }
 loadingAnimation();
 
-function scrollTrigger(element, scale, opacity, delay, duration, y, width) {
+gsap.to('.nav-left svg', {
+  transform: "translateY(-100%)",
+  scrollTrigger: {
+    trigger: "#page-1",
+    scroller: ".main",
+    start: "top 0",
+    end: "top -5%",
+    scrub: true,
+  }
+})
+gsap.to('.nav-right .nav-links', {
+  transform: "translateY(-100%)",
+  scrollTrigger: {
+    trigger: "#page-1",
+    opacity: 0,
+    scroller: ".main",
+    start: "top 0",
+    end: "top -5%",
+    scrub: true,
+  }
+})
+
+function scrollTrigger(element, scale, opacity, delay, duration, y, width, rotate) {
     const observer = new IntersectionObserver(entries => {
         entries.forEach(entry => {
           if (entry.isIntersecting) {
@@ -104,7 +154,8 @@ function scrollTrigger(element, scale, opacity, delay, duration, y, width) {
               delay,
               duration,
               y,
-              width
+              width,
+              rotate
             });
             // Once the animation is triggered, you may want to unobserve the element
             observer.unobserve(entry.target);
@@ -116,7 +167,7 @@ function scrollTrigger(element, scale, opacity, delay, duration, y, width) {
     if (elementToObserve) {
     observer.observe(elementToObserve);
     }
-}
+};
 scrollTrigger("#product1", scale=1.1, opacity=0, delay=0.2, duration=0.4);
 scrollTrigger("#product2", scale=1.1, opacity=0, delay=0.4, duration=0.4);
 scrollTrigger("#product3", scale=1.1, opacity=0, delay=0.6, duration=0.4);
@@ -134,6 +185,11 @@ scrollTrigger(element=".line-4", scale=1, opacity=0, delay=0.5, duration=0.2, y=
 scrollTrigger(element=".line-5", scale=1, opacity=0, delay=0.6, duration=0.2, y=100)
 scrollTrigger(element=".impact-img1", scale=1.1, opacity=0, delay=0.2, duration=0.4, y=100)
 scrollTrigger(element=".impact-img2", scale=1.1, opacity=0, delay=0.3, duration=0.4, y=100)
+scrollTrigger(element=".email-container h3", scale=1, opacity=0, delay=0.2, duration=0.4, y=100)
+scrollTrigger(element=".email-svg-container img", scale=0, opacity=0, delay=0.3, duration=0.4, 0, 0, rotate=360)
+scrollTrigger(element=".logo-container", scale=1, opacity=0, delay=0.1, duration=0.2, y=100)
+
+
 
 function cursorAnimation(cursorType, element) {
     document.addEventListener("mousemove", function(dets) {
@@ -156,7 +212,7 @@ function cursorAnimation(cursorType, element) {
             transform: "translate(-50%, -50%) scale(0)"
         })
     })
-}
+};
 
 cursorAnimation(".cursor.almond","#g-products1");
 cursorAnimation(".cursor.gray","#g-products2");
@@ -165,53 +221,59 @@ cursorAnimation(".cursor.orange","#g-products4");
 cursorAnimation(".cursor.orange","#g-products5");
 cursorAnimation(".cursor.red","#g-products6");
 
-const buttonContainer = document.querySelector('.button-container');
 
-let isScrolling = false;
-let startX;
-let scrollLeft;
-
-buttonContainer.addEventListener('mousedown', (e) => {
-  isScrolling = true;
-  startX = e.pageX - buttonContainer.offsetLeft;
-  scrollLeft = buttonContainer.scrollLeft;
-});
-
-buttonContainer.addEventListener('mouseleave', () => {
-  isScrolling = false;
-});
-
-buttonContainer.addEventListener('mouseup', () => {
-  isScrolling = false;
-});
-
-buttonContainer.addEventListener('mousemove', (e) => {
-  if (!isScrolling) return;
-  e.preventDefault();
-  const x = e.pageX - buttonContainer.offsetLeft;
-  const walk = (x - startX) * 2;
+function scrollDragButtonsAnimation() {
+  const buttonContainer = document.querySelector('.button-container');
   
-  requestAnimationFrame(() => {
-    buttonContainer.scrollLeft = scrollLeft - walk;
+  let isScrolling = false;
+  let startX;
+  let scrollLeft;
+  
+  buttonContainer.addEventListener('mousedown', (e) => {
+    isScrolling = true;
+    startX = e.pageX - buttonContainer.offsetLeft;
+    scrollLeft = buttonContainer.scrollLeft;
   });
-});
-
-// New: Scroll to the middle when clicking a button
-const buttons = document.querySelectorAll('.button-container button');
-
-buttons.forEach((button) => {
-  button.addEventListener('click', () => {
-    const buttonRect = button.getBoundingClientRect();
-    const containerRect = buttonContainer.getBoundingClientRect();
-    const scrollAmount = buttonRect.left + buttonRect.width / 2 - containerRect.left - containerRect.width / 2;
-    buttonContainer.scrollLeft += scrollAmount;
-
-    buttonContainer.scrollTo({
-      left: buttonContainer.scrollLeft + scrollAmount,
-      behavior: 'smooth'
+  
+  buttonContainer.addEventListener('mouseleave', () => {
+    isScrolling = false;
+  });
+  
+  buttonContainer.addEventListener('mouseup', () => {
+    isScrolling = false;
+  });
+  
+  buttonContainer.addEventListener('mousemove', (e) => {
+    if (!isScrolling) return;
+    e.preventDefault();
+    const x = e.pageX - buttonContainer.offsetLeft;
+    const walk = (x - startX) * 2;
+    
+    requestAnimationFrame(() => {
+      buttonContainer.scrollLeft = scrollLeft - walk;
     });
   });
-});
+
+    // New: Scroll to the middle when clicking a button
+    const buttons = document.querySelectorAll('.button-container button');
+  
+    buttons.forEach((button) => {
+      button.addEventListener('click', () => {
+        const buttonRect = button.getBoundingClientRect();
+        const containerRect = buttonContainer.getBoundingClientRect();
+        const scrollAmount = buttonRect.left + buttonRect.width / 2 - containerRect.left - containerRect.width / 2;
+        buttonContainer.scrollLeft += scrollAmount;
+    
+        buttonContainer.scrollTo({
+          left: buttonContainer.scrollLeft + scrollAmount,
+          behavior: 'smooth'
+        });
+      });
+    });
+
+};
+
+scrollDragButtonsAnimation();
 
 function resetButtonAnimation() {
   const buttons = document.querySelectorAll('.button-container button');
@@ -237,7 +299,7 @@ function resetButtonAnimation() {
       }
     });
   });
-}
+};
 
 resetButtonAnimation();
 
@@ -267,7 +329,8 @@ function UnderlineAnimation() {
       }
     });
   });
-}
+};
+
 UnderlineAnimation();
 
 
